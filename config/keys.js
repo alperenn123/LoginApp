f6f29633b6b8edff01d502ef13f9185ab54d2d0c
Alperen SALAR <alperenn123@gmail.com>
2020-10-27 00:59:50 +0300

    Fixes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://alperenn123:alpi65719196@cluster0.t4ntl.mongodb.net/<dbname>?retryWrites=true&w=majority'
-}
+  MongoURI:
+    "mongodb+srv://username:password@cluster0.t4ntl.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
